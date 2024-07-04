dc2fbb4db21127430d40fb73bf50d640c5ffec6f
iogann <31204862+iagann@users.noreply.github.com>
2024-07-04 15:16:02 +0000

    sandbox setup

---
+++ build/makeBuildGood/CMakeFiles/makeBuildGood.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/project/workspace/makeBuildGood/BuildMaker.cpp" "/project/workspace/build/makeBuildGood/CMakeFiles/makeBuildGood.dir/BuildMaker.cpp.o"
+  "/project/workspace/makeBuildGood/FileUtil.cpp" "/project/workspace/build/makeBuildGood/CMakeFiles/makeBuildGood.dir/FileUtil.cpp.o"
+  "/project/workspace/makeBuildGood/Item.cpp" "/project/workspace/build/makeBuildGood/CMakeFiles/makeBuildGood.dir/Item.cpp.o"
+  "/project/workspace/makeBuildGood/enums.cpp" "/project/workspace/build/makeBuildGood/CMakeFiles/makeBuildGood.dir/enums.cpp.o"
+  "/project/workspace/makeBuildGood/makeBuildGood.cpp" "/project/workspace/build/makeBuildGood/CMakeFiles/makeBuildGood.dir/makeBuildGood.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNICODE"
+  "_CONSOLE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
